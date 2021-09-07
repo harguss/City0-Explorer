53c66db3e71b43ffc00058b3de37d99fb6023335
SHargus <87950738+harguss@users.noreply.github.com>
2021-09-07 16:42:17 -0500

    scaffolding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label>
+          Search for a location:
+          {' '} {/* add a space between */}
+          <input type="text" name="search" placeholder="Location" />
+        </label>
+        <div>
+          <button type="submit">Search</button>
+        </div>
+      </form>
     </div>
   );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <form>
+          <label>
+            Search for a location:
+            {' '} {/* add a space between */}
+            <input type="text" name="search" placeholder="Location" />
+          </label>
+          <div>
+            <button type="submit">Search</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
